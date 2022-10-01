dbd4de07d878df169944b21ec777ff4026cfcc0d
Richard Janssen <richard.janssen.93@gmail.com>
2022-10-01 17:10:32 +0200

    Add linting and

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -5,11 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class AdminComponent {
 
 }
