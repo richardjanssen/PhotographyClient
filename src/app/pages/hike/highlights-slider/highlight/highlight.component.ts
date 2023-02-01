c90f6fa8ae3c4d0b65767de33461f4fe68db886a
Richard Janssen <richard.janssen.93@gmail.com>
2023-02-01 21:50:05 +0100

    Add connectors between highlights

--- src/app/pages/hike/highlights-slider/highlight/highlight.component.ts
+++ src/app/pages/hike/highlights-slider/highlight/highlight.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Highlight } from 'src/app/core/types/highlight.type';
 
 @Component({
@@ -6,26 +6,6 @@ import { Highlight } from 'src/app/core/types/highlight.type';
     templateUrl: './highlight.component.html',
     styleUrls: ['./highlight.component.scss']
 })
-export class HighlightComponent implements AfterViewInit {
-    @ViewChild('title') title: ElementRef;
-    @ViewChild('titleContainer') titleContainer: ElementRef;
-    @ViewChild('titleSpacerLeft') titleSpacerLeft: ElementRef;
-    @ViewChild('titleSpacerRight') titleSpacerRight: ElementRef;
+export class HighlightComponent {
     @Input() highlight: Highlight;
-    spacerWidth: number = 0;
-
-    constructor(private readonly _changeDetectorRef: ChangeDetectorRef) {}
-
-    ngAfterViewInit(): void {
-        const originalHeight = this.titleContainer.nativeElement.offsetHeight;
-
-        while (originalHeight === this.titleContainer.nativeElement.offsetHeight) {
-            ++this.spacerWidth;
-            this._changeDetectorRef.detectChanges();
-        }
-
-        --this.spacerWidth;
-        --this.spacerWidth; // Needs to be done twice due to some rounding weirdness
-        this._changeDetectorRef.detectChanges();
-    }
 }
