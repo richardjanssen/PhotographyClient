b8d45fe5676e0b49d6436c6bf646ef3bf4c983d0
Richard Janssen <richard.janssen.93@gmail.com>
2023-02-23 22:17:43 +0100

    Scroll to active highlight

--- src/app/core/services/highlight.service.ts
+++ src/app/core/services/highlight.service.ts
@@ -8,79 +8,109 @@ import { Highlight, HighlightType } from '../types/highlight.type';
 export class HightlightService {
     private readonly _highlights: Highlight[] = [
         {
+            id: 1,
             title: 'This is a location',
             distance: 5,
-            type: HighlightType.other
+            type: HighlightType.other,
+            currentLocation: false
         },
         {
+            id: 2,
             title: 'Here you have another place',
             distance: 23,
-            type: HighlightType.photos
+            type: HighlightType.photos,
+            currentLocation: false
         },
         {
+            id: 3,
             title: 'Very beautiful',
             distance: 42,
-            type: HighlightType.town
+            type: HighlightType.town,
+            currentLocation: false
         },
         {
+            id: 4,
             title: 'Shorty',
             distance: 76,
-            type: HighlightType.other
+            type: HighlightType.other,
+            currentLocation: false
         },
         {
+            id: 5,
             title: 'A bay',
             distance: 98,
-            type: HighlightType.other
+            type: HighlightType.other,
+            currentLocation: false
         },
         {
+            id: 6,
             title: 'Very beautiful',
             distance: 100,
-            type: HighlightType.other
+            type: HighlightType.other,
+            currentLocation: false
         },
         {
+            id: 7,
             title: 'This is a location',
             distance: 123,
-            type: HighlightType.other
+            type: HighlightType.other,
+            currentLocation: false
         },
         {
+            id: 8,
             title: 'Yosemite National Park',
             distance: 130,
-            type: HighlightType.town
+            type: HighlightType.town,
+            currentLocation: true
         },
         {
+            id: 9,
             title: 'Very beautiful',
             distance: 143,
-            type: HighlightType.other
+            type: HighlightType.other,
+            currentLocation: false
         },
         {
+            id: 10,
             title: 'This is a location',
             distance: 150,
-            type: HighlightType.other
+            type: HighlightType.other,
+            currentLocation: false
         },
         {
-            title: 'Here you have another place',
+            id: 11,
+            title: 'You are here',
             distance: 176,
-            type: HighlightType.other
+            type: HighlightType.other,
+            currentLocation: true
         },
         {
+            id: 12,
             title: 'Very beautiful',
             distance: 200,
-            type: HighlightType.other
+            type: HighlightType.other,
+            currentLocation: false
         },
         {
+            id: 13,
             title: 'This is a location',
             distance: 299,
-            type: HighlightType.other
+            type: HighlightType.other,
+            currentLocation: false
         },
         {
+            id: 14,
             title: 'Here you have another place',
             distance: 309,
-            type: HighlightType.other
+            type: HighlightType.other,
+            currentLocation: false
         },
         {
+            id: 15,
             title: 'Very beautiful',
             distance: 343,
-            type: HighlightType.other
+            type: HighlightType.other,
+            currentLocation: false
         }
     ];
 
--- src/app/pages/hike/highlights-chain/highlights-chain.component.ts
+++ src/app/pages/hike/highlights-chain/highlights-chain.component.ts
@@ -1,3 +1,4 @@
+import { ViewportScroller } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { fromEvent } from 'rxjs';
 import { HightlightService } from 'src/app/core/services/highlight.service';
@@ -13,11 +14,16 @@ export class HighlightsChainComponent implements AfterViewInit, OnInit {
 
     highlights: Highlight[];
 
-    constructor(readonly highlightService: HightlightService, private renderer: Renderer2) {
+    constructor(
+        readonly highlightService: HightlightService,
+        private readonly renderer: Renderer2,
+        private readonly viewportScroller: ViewportScroller
+    ) {
         highlightService.getHighlights().subscribe(result => (this.highlights = result));
     }
 
     ngOnInit(): void {
+        this.viewportScroller.setOffset([0, 100]);
         fromEvent(window, 'resize').subscribe(() => {
             this.setHighlightsWidth();
         });
@@ -25,10 +31,22 @@ export class HighlightsChainComponent implements AfterViewInit, OnInit {
 
     ngAfterViewInit(): void {
         this.setHighlightsWidth();
+
+        setTimeout(() => {
+            this.scrollToActiveHighlight();
+        }, 1000);
     }
 
     private setHighlightsWidth(): void {
         const containerWidth = this.highlightsChain.nativeElement.offsetWidth;
         this.renderer.setProperty(document.documentElement, 'style', `--highlights-width: ${containerWidth}px`);
+    }
+
+    private scrollToActiveHighlight(): void {
+        const activeHighlight = this.highlights.find(highlight => highlight.currentLocation);
+
+        if (activeHighlight) {
+            this.viewportScroller.scrollToAnchor(activeHighlight.id.toString());
+        }
     }
 }
