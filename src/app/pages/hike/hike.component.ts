65600def391d164f244d1f21f81cc036282954d1
Richard Janssen <richard.janssen.93@gmail.com>
2023-03-04 22:09:19 +0100

    Change color scheme

--- src/app/pages/hike/hike.component.ts
+++ src/app/pages/hike/hike.component.ts
@@ -1,6 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, Renderer2 } from '@angular/core';
 
 @Component({
     templateUrl: './hike.component.html'
 })
-export class HikeComponent {}
+export class HikeComponent implements OnDestroy {
+    backgroundClass = 'green';
+
+    constructor(private readonly renderer: Renderer2) {
+        this.renderer.addClass(document.body, this.backgroundClass);
+    }
+
+    ngOnDestroy(): void {
+        this.renderer.removeClass(document.body, this.backgroundClass);
+    }
+}
