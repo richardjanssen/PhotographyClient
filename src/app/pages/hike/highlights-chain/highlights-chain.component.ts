7c432b395820e5f8ef3925bea8a6e9563250a224
Richard Janssen <richard.janssen.93@gmail.com>
2023-04-16 16:51:10 +0200

    Move section highlight to separate component

--- src/app/pages/hike/highlights-chain/highlights-chain.component.ts
+++ src/app/pages/hike/highlights-chain/highlights-chain.component.ts
@@ -2,8 +2,7 @@ import { ViewportScroller } from '@angular/common';
 import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { fromEvent } from 'rxjs';
 import { HightlightService } from 'src/app/core/services/highlight.service';
-import { StyleService } from 'src/app/core/services/style.service';
-import { AlignmentType, HighlightExpansion, Highlight, HighlightType } from 'src/app/core/types/highlight.type';
+import { AlignmentType, Highlight, HighlightType } from 'src/app/core/types/highlight.type';
 
 @Component({
     selector: 'app-highlights-chain',
@@ -15,17 +14,13 @@ export class HighlightsChainComponent implements AfterViewInit, OnInit {
 
     highlights: Highlight[];
     highlightAlignments: AlignmentType[];
-    private expandedPointHighlights: { sectionIndex: number; pointIndex: number }[] = [];
-    expandedSectionHighlightIds: number[] = [];
-    resizingSectionHighlightIds: number[] = [];
     highlightType: typeof HighlightType = HighlightType;
     alignmentType: typeof AlignmentType = AlignmentType;
 
     constructor(
         readonly highlightService: HightlightService,
         private readonly renderer: Renderer2,
-        private readonly viewportScroller: ViewportScroller,
-        private readonly _styleService: StyleService
+        private readonly viewportScroller: ViewportScroller
     ) {
         highlightService.getHighlights().subscribe(result => {
             this.highlightAlignments = this.getHighlightAlignments(result);
@@ -44,44 +39,8 @@ export class HighlightsChainComponent implements AfterViewInit, OnInit {
         this.setHighlightsWidth();
     }
 
-    trackHighlightExpansion(expansion: HighlightExpansion): void {
-        if (expansion.isExpanded) {
-            this.expandedPointHighlights.push({ sectionIndex: expansion.sectionIndex, pointIndex: expansion.pointIndex });
-        } else {
-            this.expandedPointHighlights = this.expandedPointHighlights.filter(
-                highlight => highlight.sectionIndex !== expansion.sectionIndex || highlight.pointIndex !== expansion.pointIndex
-            );
-        }
-        this.updateSectionHighlightExpansions(expansion);
-    }
-
     scrollToCurrentLocation(): void {
         this.viewportScroller.scrollToAnchor('currentLocation');
-    }
-
-    private updateSectionHighlightExpansions(expansion: HighlightExpansion): void {
-        // Get section highlight that contains toggled expansion
-        const sectionHighlight = this.highlights.find(highlight => highlight.sectionHighlight?.highlightIndex === expansion.sectionIndex);
-        const sectionHighlightIndex = sectionHighlight!.sectionHighlight!.highlightIndex;
-
-        // If at least one section child is expanded and section is not already been added,
-        // then add section highlight to expanded section highlights
-        if (this.expandedPointHighlights.some(highlight => highlight.sectionIndex === sectionHighlightIndex)) {
-            if (!this.expandedSectionHighlightIds.some(id => id === sectionHighlightIndex)) {
-                this.resizingSectionHighlightIds.push(sectionHighlightIndex);
-                this.expandedSectionHighlightIds.push(sectionHighlightIndex);
-                setTimeout(() => {
-                    this.resizingSectionHighlightIds = this.expandedSectionHighlightIds.filter(id => id !== sectionHighlightIndex);
-                }, this._styleService.transitionTimeMs);
-            }
-            // Remove section highlight from expanded sections highlights if no childs are expanded
-        } else {
-            this.resizingSectionHighlightIds.push(sectionHighlightIndex);
-            this.expandedSectionHighlightIds = this.expandedSectionHighlightIds.filter(id => id !== sectionHighlightIndex);
-            setTimeout(() => {
-                this.resizingSectionHighlightIds = this.expandedSectionHighlightIds.filter(id => id !== sectionHighlightIndex);
-            }, this._styleService.transitionTimeMs);
-        }
     }
 
     private setHighlightsWidth(): void {
--- src/app/pages/hike/highlights-chain/section-highlight/section-highlight.component.ts
+++ src/app/pages/hike/highlights-chain/section-highlight/section-highlight.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { AlignmentType, Highlight } from 'src/app/core/types/highlight.type';
+import { StyleService } from 'src/app/core/services/style.service';
+import { AlignmentType, HighlightExpansion, SectionHighlight } from 'src/app/core/types/highlight.type';
 
 @Component({
     selector: 'app-section-highlight',
@@ -7,12 +8,46 @@ import { AlignmentType, Highlight } from 'src/app/core/types/highlight.type';
     styleUrls: ['./section-highlight.component.scss']
 })
 export class SectionHighlightComponent implements OnInit {
-    @Input() highlight: Highlight;
+    @Input() highlight: SectionHighlight;
     @Input() alignment: AlignmentType;
+
+    private expandedPointHighlights: number[] = [];
+
+    expanded: boolean;
+    resizing: boolean;
 
     alignmentType: typeof AlignmentType = AlignmentType;
 
-    constructor() {}
+    constructor(private readonly _styleService: StyleService) {}
 
     ngOnInit(): void {}
+
+    trackHighlightExpansion(expansion: HighlightExpansion): void {
+        if (expansion.isExpanded) {
+            this.expandedPointHighlights.push(expansion.pointIndex);
+        } else {
+            this.expandedPointHighlights = this.expandedPointHighlights.filter(index => index !== expansion.pointIndex);
+        }
+        this.toggleSectionExpansion();
+    }
+
+    private toggleSectionExpansion(): void {
+        // If at least one child is expanded and section is not currently expanded, then expand section
+        // Collapse section when there are no expanded childs
+        if (this.expandedPointHighlights.length > 0) {
+            if (!this.expanded) {
+                this.resizing = true;
+                this.expanded = true;
+                setTimeout(() => {
+                    this.resizing = false;
+                }, this._styleService.transitionTimeMs);
+            }
+        } else {
+            this.resizing = true;
+            this.expanded = false;
+            setTimeout(() => {
+                this.resizing = false;
+            }, this._styleService.transitionTimeMs);
+        }
+    }
 }
