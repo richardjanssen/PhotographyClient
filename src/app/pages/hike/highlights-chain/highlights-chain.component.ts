8c3ce7c41312a6b7423cc7393d9935cd57aebc22
Richard Janssen <richard.janssen.93@gmail.com>
2023-04-17 20:32:58 +0200

    Retrieve highlights in hike and remove bootstrap

--- src/app/pages/hike/highlights-chain/highlights-chain.component.ts
+++ src/app/pages/hike/highlights-chain/highlights-chain.component.ts
@@ -1,7 +1,6 @@
 import { ViewportScroller } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { fromEvent } from 'rxjs';
-import { HightlightService } from 'src/app/core/services/highlight.service';
 import { AlignmentType, Highlight, HighlightType } from 'src/app/core/types/highlight.type';
 
 @Component({
@@ -12,23 +11,15 @@ import { AlignmentType, Highlight, HighlightType } from 'src/app/core/types/high
 export class HighlightsChainComponent implements AfterViewInit, OnInit {
     @ViewChild('highlightsChain') highlightsChain: ElementRef;
 
-    highlights: Highlight[];
+    @Input() highlights: Highlight[];
     highlightAlignments: AlignmentType[];
     highlightType: typeof HighlightType = HighlightType;
     alignmentType: typeof AlignmentType = AlignmentType;
 
-    constructor(
-        readonly highlightService: HightlightService,
-        private readonly renderer: Renderer2,
-        private readonly viewportScroller: ViewportScroller
-    ) {
-        highlightService.getHighlights().subscribe(result => {
-            this.highlightAlignments = this.getHighlightAlignments(result);
-            this.highlights = result;
-        });
-    }
+    constructor(private readonly renderer: Renderer2, private readonly viewportScroller: ViewportScroller) {}
 
     ngOnInit(): void {
+        this.highlightAlignments = this.getHighlightAlignments(this.highlights);
         this.viewportScroller.setOffset([0, 100]);
         fromEvent(window, 'resize').subscribe(() => {
             this.setHighlightsWidth();
@@ -44,8 +35,10 @@ export class HighlightsChainComponent implements AfterViewInit, OnInit {
     }
 
     private setHighlightsWidth(): void {
-        const containerWidth = this.highlightsChain.nativeElement.offsetWidth;
-        this.renderer.setProperty(document.documentElement, 'style', `--highlights-width: ${containerWidth}px`);
+        if (this.highlights) {
+            const containerWidth = this.highlightsChain.nativeElement.offsetWidth;
+            this.renderer.setProperty(document.documentElement, 'style', `--highlights-width: ${containerWidth}px`);
+        }
     }
 
     private getHighlightAlignments(highlights: Highlight[]): AlignmentType[] {
