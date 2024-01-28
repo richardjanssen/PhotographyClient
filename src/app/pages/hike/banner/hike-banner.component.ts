8af426ce81a6e63220a41ad7e1f959b9a5bfcd9c
Richard Janssen <richard.janssen.93@gmail.com>
2024-01-28 21:06:15 +0100

    Change banner text, map fullscreen, explore map backup link

--- src/app/pages/hike/banner/hike-banner.component.ts
+++ src/app/pages/hike/banner/hike-banner.component.ts
@@ -1,9 +1,19 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'hike-banner',
     templateUrl: './hike-banner.component.html',
     styleUrls: ['./hike-banner.component.scss'],
-    standalone: true
+    standalone: true,
+    imports: [NgIf]
 })
-export class HikeBannerComponent {}
+export class HikeBannerComponent {
+    hasStartedHike: boolean;
+    constructor() {
+        const startDate = new Date(2024, 3, 15); // 15 April 2024
+        if (Date.now() > startDate.valueOf()) {
+            this.hasStartedHike = true;
+        }
+    }
+}
