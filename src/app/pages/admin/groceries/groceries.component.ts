db5f9e4555761656827a042ed568a429fa3533ff
Richard Janssen <github@rjanssen.net>
2026-08-07 09:21:07 +0200

    build error fix

--- src/app/pages/admin/groceries/groceries.component.ts
+++ src/app/pages/admin/groceries/groceries.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RecipesPaths } from './groceries.routes';
+
 import { BaseLayoutComponent } from "src/app/core/components/base-layout/base-layout.component";
 
 @Component({
@@ -8,5 +8,4 @@ import { BaseLayoutComponent } from "src/app/core/components/base-layout/base-la
     imports: [BaseLayoutComponent]
 })
 export class GroceriesComponent {
-    readonly children: { route: string; title: string }[] = [{ route: RecipesPaths.add, title: 'Recept toevoegen' }];
 }
