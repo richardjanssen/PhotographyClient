fbdd6cbf25ce9c374b0d16729b36f9bdb8914fa7
Richard Janssen <github@rjanssen.net>
2026-08-10 19:38:47 +0200

    Error message, kleinere admin knoppen

--- src/app/pages/admin/recipes/add-recipe/add-recipe.component.ts
+++ src/app/pages/admin/recipes/add-recipe/add-recipe.component.ts
@@ -50,7 +50,6 @@ export class AddRecipeComponent implements OnInit {
     ingredientGroupForms: FormArray<FormGroup<IngredientGroupForm>> = new FormArray<FormGroup<IngredientGroupForm>>([]);
 
     submitted: boolean = false;
-    success: boolean = false;
     error: boolean = false;
 
     constructor(private readonly _recipeService: RecipeService, private readonly router: Router) {}
@@ -135,6 +134,7 @@ export class AddRecipeComponent implements OnInit {
     }
 
     onSubmit(): void {
+        this.error = false;
         this.recipeForm.markAllAsTouched();
         this.ingredientGroupForms.markAllAsTouched();
 
